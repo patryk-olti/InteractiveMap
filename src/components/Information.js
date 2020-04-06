88ee0e95cb3919d6e979d7d346ac630084a1f886
Patryk Oltuch <oltix@wp.pl>
2020-04-06 15:06:02 +0200

    last commit of the day

---
+++ src/components/Information.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Information.css';
+
+function Information(){
+
+    return(
+        <div className="Information">
+            <div>Elektrociepłownia Wrocław</div>
+            
+            
+        </div>
+    )
+}
+
+export default Information;
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,19 +1,52 @@
 import React from 'react';
 import map from "./img/mapa.png";
 import "./Map.css";
+import Information from './Information';
 
-function Map(){
+class Map extends React.Component{
+
+    state = {
+        iteration: 1,
+        maxIteration: 5,
+        base: []
+    }
+
+    changeActive = () => {
+        const dots = document.querySelectorAll('.dot');
+
+        dots.forEach(item => {
+            item.classList.toggle('active');
+        });
 
 
+    }
 
-    return (
-        <div className="polandMap">
+    componentDidMount(){
+        fetch('data.json')
+        .then(response => response.json())
+        .then(data => {
+            setInterval( this.changeActive() , 2000);
 
-            <div className="dot wroclaw active"></div>
-            <div className="dot szczecin active"></div>
-            <img src={map} alt="map"/>
-        </div>
-    )
+            this.setState({
+                base: data.devices
+            })
+        })  
+    }
+
+    render(){
+        console.log(this.state.base);
+
+        return (
+            <div className="polandMap">
+    
+                <div className="dot wroclaw active"></div>
+                <div className="dot szczecin active"></div>
+                <img src={map} alt="map"/>
+    
+                <Information />
+            </div>
+        )
+    }
 }
 
 export default Map;
